614449165616e5be01edb969e6aae5ea8fb4e2c8
Emaan <emaanjawad15@gmail.com>
2024-05-27 10:16:53 +0500

    Project .

--- Complete Project/routes/site/productshow.js
+++ Complete Project/routes/site/productshow.js
@@ -6,26 +6,9 @@ const Books = require("../../models/Books");
 router.get("/books/productdisplay", async (req, res) => {
     let books = await Books.find();
     console.log(books);
-    res.render("books/productdisplay", { pageTitle: "All Books", books , page:1, totalPages:1});
+    res.render("books/productdisplay", { pageTitle: "All Books", books});
 
   });
   
-router.get("/products/:page?", async (req, res) => {
-    let page = Number(req.params.page) ? Number(req.params.page) : 1;
-    let pageSize = 6;
-    let books = await Books.find()
-      .skip(pageSize * (page - 1))
-      .limit(pageSize);
-    let total = await Books.countDocuments();
-    let totalPages = Math.ceil(total / pageSize);
-    res.render("books/productdisplay", {
-      pageTitle: "Products: ",
-      books,
-      total,
-      page,
-      pageSize,
-      totalPages
-    });
-  });
 
   module.exports=router;
